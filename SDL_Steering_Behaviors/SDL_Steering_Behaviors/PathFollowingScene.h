8c4e31f7940d9305491b4c4a6a0dbd3ccfc5db08
Aniol Folch <aniolfolchaloy@enti.cat>
2017-10-06 11:30:21 +0200

    Added PathFollowingScene cpp and header file

---
+++ SDL_Steering_Behaviors/SDL_Steering_Behaviors/PathFollowingScene.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <vector>
+#include "Scene.h"
+#include "Agent.h"
+#include "Image.h"
+
+class PathFollowingScene :
+	public Scene
+{
+public:
+	PathFollowingScene();
+	~PathFollowingScene();
+	void update(float dtime, SDL_Event *event);
+	void draw();
+	const char* getTitle();
+private:
+	std::vector<Agent*> agents;
+	Image* text;
+	Vector2D target;
+};
---
+++ SDL_Steering_Behaviors/src/PathFollowingScene.cpp
@@ -0,0 +1,50 @@
+#include "PathFollowingScene.h"
+
+using namespace std;
+
+PathFollowingScene::PathFollowingScene() {
+	Agent *agent = new Agent;
+	agent->setPosition(Vector2D(640, 360));
+	agent->setTarget(Vector2D(640, 360));
+	agent->loadSpriteTexture("../res/soldier.png", 4);
+	agents.push_back(agent);
+	target = Vector2D(640, 360);
+	text = new Image(Vector2D(TheApp::Instance()->getWinSize().x / 2, 100));
+	text->LoadImage("../res/Text/fleeDemo.png");
+}
+
+PathFollowingScene::~PathFollowingScene() {
+	for (int i = 0; i < (int)agents.size(); i++)
+	{
+		delete agents[i];
+	}
+	delete text;
+}
+
+void PathFollowingScene::update(float dtime, SDL_Event *event) {
+	// Keyboard & Mouse events
+	switch (event->type) {
+	case SDL_MOUSEMOTION:
+	case SDL_MOUSEBUTTONDOWN:
+		if (event->button.button == SDL_BUTTON_LEFT)
+		{
+			target = Vector2D((float)(event->button.x), (float)(event->button.y));
+			agents[0]->setTarget(target);
+		}
+		break;
+	default:
+		break;
+	}
+	Vector2D steering_force = agents[0]->Behavior()->Flee(agents[0], agents[0]->getTarget(), dtime);
+	agents[0]->update(steering_force, dtime, event);
+}
+
+void PathFollowingScene::draw() {
+	draw_circle(TheApp::Instance()->getRenderer(), (int)target.x, (int)target.y, 15, 255, 0, 0, 255);
+	agents[0]->draw();
+	text->Draw();
+}
+
+const char* PathFollowingScene::getTitle() {
+	return "SDL Steering Behaviors :: Flee Demo";
+}
